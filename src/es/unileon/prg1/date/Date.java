7bd192b958beecedae66a2f537cd1a066c93e05c
apereg21 <apereg21@estudiantes.unileon.es>
2018-03-26 12:55:10 +0200

    Cambios a la practica Date

--- src/es/unileon/prg1/date/Date.java
+++ src/es/unileon/prg1/date/Date.java
@@ -27,4 +27,54 @@ public class Date {
 		return this.day + "/" + this.month + "/" + this.year;
 	}
 
+	public String getMonthName(){
+	String name=null;
+	switch(this.month){
+	case 1:
+		name="Enero";
+	break;
+	case 2:
+		name="Febrero";
+	break;
+	case 3:
+		name="Marzo";
+	break;
+	case 4:
+		name="Abril";
+	break;
+	case 5:
+		name="Mayo";
+	break;
+	case 6:
+		name="Junio";
+	break;
+	case 7:
+		name="Julio";
+	break;
+	case 8:
+		name="Agosto";
+	break;
+	case 9:
+		name="Septiembre";
+	break;
+	case 10:
+		name="Octubre";
+	break;
+	case 11:
+		name="Noviembre";
+	break;
+	case 12:
+		name="Diciembre";
+	break;
+	}
+	return name;
+	}
+	
+	boolean isDayOfMonthOK(){
+	boolean ok;
+	}
+	
+	public String monthsLeft(){
+	
+	}
 }
