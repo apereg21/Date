d3ed75971d1d82f2be8ec2d54aac25e0d0d90fec
apereg21 <apereg21@estudiantes.unileon.es>
2018-03-26 17:04:31 +0200

    Avance en la clase Date

--- src/es/unileon/prg1/date/Date.java
+++ src/es/unileon/prg1/date/Date.java
@@ -15,6 +15,14 @@ public class Date {
 	public int getYear(){
 		return this.year;
 	}
+	
+	public int getMonth(){
+		return this.month;
+	}
+
+	public int getDay(){
+		return this.day;
+	}
 
 	boolean isSameYear(Date another){
 		if ( this.year == another.getYear() ){
@@ -22,7 +30,27 @@ public class Date {
 		}
 		return false;
 	}
+
+	boolean isSameMonth(Date another){
+		if ( this.month == another.getMonth() ){
+			return true;
+		}
+		return false;
+	}
 	
+	boolean isSameDay(Date another){
+		if ( this.day == another.getDay() ){
+			return true;
+		}
+		return false;
+	}
+	
+	boolean isSame(Date another){
+		if ( (this.day == another.getDay()) && (this.month == another.getMonth()) && (this.year == another.getYear()) ){
+			return true;
+		}
+		return false;
+	}
 	public String toString(){
 		return this.day + "/" + this.month + "/" + this.year;
 	}
@@ -70,11 +98,64 @@ public class Date {
 	return name;
 	}
 	
-	boolean isDayOfMonthOK(){
-	boolean ok;
+	public String getMonthSeason(){
+	String name=null;
+	switch(this.month){
+	case 1://next
+	case 2://next
+	case 3://next
+		name="Invierno";
+	break;
+	case 4://next
+	case 5://next
+	case 6://next
+		name="Primavera";
+	break;
+	case 7://next
+	case 8://next
+	case 9://next
+		name="Verano";
+	break;
+	case 10://next
+	case 11://next
+	case 12://next
+		name="Otonio";
+	break;
+	}
+	return name;
 	}
 	
-	public String monthsLeft(){
-	
+	boolean isMonthDayOk(){
+	boolean ok=false;
+	switch(this.month){
+	case 1://next
+	case 3://next
+	case 5://next
+	case 7://next
+	case 8://next
+	case 10://next
+	case 12://next
+		if(this.day<=31){
+		ok=true;
+		}		ok=false;	
+	break;
+	case 2://next
+		if(this.day<=28){
+		ok=true;
+		}
+		ok=false;	
+	break;
+	case 4://next
+	case 6://next
+	case 9://next
+	case 11://next
+		if(this.day<=30){
+		ok=true;
+		}
+		ok=false;	
+	break;
 	}
+	return ok;
+	}
+
 }
--- src/es/unileon/prg1/date/MainDate.java
+++ src/es/unileon/prg1/date/MainDate.java
@@ -7,7 +7,13 @@ public class MainDate {
 		today = new Date(20, 3, 2018);
 		tomorrow = new Date(21, 3, 2018);
 		System.out.println(today + " isSameYear " + tomorrow + "? " + today.isSameYear(tomorrow));
+		System.out.println(today + " isSameMonth " + tomorrow + "? " + today.isSameMonth(tomorrow));
+		System.out.println(today + " isSameDay " + tomorrow + "? " + today.isSameDay(tomorrow));
+		System.out.println(today + " isSame " + tomorrow + "? " + today.isSame(tomorrow));
 		System.out.println("El mes es: "+tomorrow.getMonthName());
+		System.out.println("¿El dia "+tomorrow.getDay()+"del mes "+tomorrow.getMonth()+" es correcto? "+tomorrow.isMonthDayOk());
+		System.out.println("¿El dia "+today.getDay()+"del mes "+today.getMonth()+" es correcto? "+today.isMonthDayOk());
+		System.out.println("La estación del mes "+tomorrow.getMonthName()+" es:"+tomorrow.getMonthSeason());
 	}
 
 }
